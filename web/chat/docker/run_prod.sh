652b62baf298dfac0d3c9eda6d5cb99f27bbccd0
Mike Murray <mikmurr@amazon.com>
2019-05-01 14:00:09 -0700

    add s3 urls to run_prod script

--- web/chat/docker/run_prod.sh
+++ web/chat/docker/run_prod.sh
@@ -2,4 +2,10 @@
 
 # Run both docker containers in the background
 docker run -d -v $(pwd)/www/client:/client -v $(pwd)/www/log:/log vln:chat-server
-docker run -d -v $(pwd)/www:/var/www/site -p 80:80 vln:chat-www
+docker run -d \
+    -v $(pwd)/www/client:/var/www/site/client -v $(pwd)/www/log:/var/www/site/log \
+    -e "R2R_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data/v1/r2r" \
+    -e "CONNECTIVITY_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data/v1/connectivity" \
+    -e "MATTERPORT_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data" \
+    -p 80:80 vln:chat-www
+
