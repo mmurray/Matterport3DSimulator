e4ce1c0d0020a1c3e087d3076e2088f1336ac4ab
Mike Murray <mmurray.wa@gmail.com>
2019-06-08 18:05:18 -0700

    use the right s3 bucket

--- web/chat/docker/run_prod.sh
+++ web/chat/docker/run_prod.sh
@@ -4,8 +4,8 @@
 docker run -d -v $(pwd)/www/client:/client -v $(pwd)/www/log:/log -v $(pwd)/house_target_tuple.json:/house_target_tuple.json vln:chat-server
 docker run -d \
     -v $(pwd)/www/client:/var/www/site/client -v $(pwd)/www/log:/var/www/site/log -v $(pwd)/www/feedback:/var/www/site/feedback \
-    -e "R2R_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data/v1/r2r" \
-    -e "CONNECTIVITY_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data/v1/connectivity" \
-    -e "MATTERPORT_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data" \
+    -e "R2R_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/mp-dialog/data/v1/r2r" \
+    -e "CONNECTIVITY_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/mp-dialog/data/v1/connectivity" \
+    -e "MATTERPORT_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/mp-dialog/data" \
     -p 80:80 vln:chat-www
 
