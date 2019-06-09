d407932631b7a352d468a83efeda32201479bb6b
Mike Murray <mmurray.wa@gmail.com>
2019-06-08 17:59:34 -0700

    mount house_target_tuple.js

--- web/chat/docker/run_prod.sh
+++ web/chat/docker/run_prod.sh
@@ -4,6 +4,7 @@
 docker run -d -v $(pwd)/www/client:/client -v $(pwd)/www/log:/log vln:chat-server
 docker run -d \
     -v $(pwd)/www/client:/var/www/site/client -v $(pwd)/www/log:/var/www/site/log -v $(pwd)/www/feedback:/var/www/site/feedback \
+    -v $(pwd)/house_target_tuple.json:/house_target_tuple.json \
     -e "R2R_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data/v1/r2r" \
     -e "CONNECTIVITY_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data/v1/connectivity" \
     -e "MATTERPORT_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/vln/data" \
