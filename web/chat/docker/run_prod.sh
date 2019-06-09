0ee8bf5d98f0e2006fabdff3c846c1c3e663f9ce
Mike Murray <mmurray.wa@gmail.com>
2019-06-08 21:08:20 -0700

    mount well_known directory for ssl cert generation

--- web/chat/docker/run_prod.sh
+++ web/chat/docker/run_prod.sh
@@ -3,7 +3,7 @@
 # Run both docker containers in the background
 docker run -d -v $(pwd)/www/client:/client -v $(pwd)/www/log:/log -v $(pwd)/house_target_tuple.json:/house_target_tuple.json vln:chat-server
 docker run -d \
-    -v $(pwd)/www/client:/var/www/site/client -v $(pwd)/www/log:/var/www/site/log -v $(pwd)/www/feedback:/var/www/site/feedback \
+    -v $(pwd)/www/client:/var/www/site/client -v $(pwd)/www/log:/var/www/site/log -v $(pwd)/www/feedback:/var/www/site/feedback  -v $(pwd)/www/.well_known:/var/www/site/.well_known \
     -e "R2R_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/mp-dialog/data/v1/r2r" \
     -e "CONNECTIVITY_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/mp-dialog/data/v1/connectivity" \
     -e "MATTERPORT_DATA_PREFIX=https://s3.us-west-2.amazonaws.com/mp-dialog/data" \
